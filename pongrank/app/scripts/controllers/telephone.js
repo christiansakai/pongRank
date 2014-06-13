f7b2c410478de9c6b45d529661341a965e3882ca
Mike Young <mikemyoung@gmail.com>
2014-06-12 20:09:52 -0400

    got rid of testuser + mongonovus added

--- pongrank/app/scripts/controllers/telephone.js
+++ pongrank/app/scripts/controllers/telephone.js
@@ -4,13 +4,15 @@ angular.module('pongrankAppTelephone', [])
 
     $http.get('/api/checkTelephone/?name=' + $scope.currentUser.name)
       .success(function(check) {
-        if (check === "true") {
+        if (check === 'true') {
           window.location = '/';
         } else {
-        $scope.loadPage = true;
+          $scope.loadPage = true;
         }
       });
+
     $scope.cellPhonePattern = /^[0-9]{10}$/g;
+
     $scope.addTelephone = function (body) {
       $http.post('/api/addTelephone/?name=' + $scope.currentUser.name + '&body=' + $scope.user.telephone)
       .success(function(user) {
--- pongrank/lib/config/auth.js
+++ pongrank/lib/config/auth.js
@@ -1,12 +1,13 @@
 var facebookId = process.env.FACEBOOK_ID;
 var facebookSecret = process.env.FACEBOOK_SECRET;
+var callbackURL = process.env.CALLBACK_URL;
 
 
 module.exports = {
     'facebookAuth' : {
         'clientID'      : facebookId, // your App ID
         'clientSecret'  : facebookSecret, // your App Secret
-        'callbackURL'   : 'http://192.168.1.185:9000/auth/facebook/callback/'
+        'callbackURL'   : callbackURL //'http://192.168.1.185:9000/auth/facebook/callback/'
     }
 
 };
